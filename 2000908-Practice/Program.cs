e6fcdd027e746b9745d983f938e8e76177c3a9be
3dapi <emarhayyim@hotmail.com>
2026-08-26 12:38:52 +0900

    프로그래밍 프로젝트 생성

---
+++ 2000908-Practice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2000908_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
