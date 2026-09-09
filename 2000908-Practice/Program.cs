44aa4bb036b7c94f6d2ae2284e63ef7a43a848c7
3dapi <emarhayyim@hotmail.com>
2026-09-09 12:21:47 +0900

    변수 연습

--- 2000908-Practice/Program.cs
+++ 2000908-Practice/Program.cs
@@ -5,11 +5,111 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("나의 이력서");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.SetCursorPosition(10, 5);
-            Console.WriteLine("이름:오희성");
+            //int a = 100;
+            //int b = 200;
+            //System.Console.WriteLine(a == b);
+            //System.Console.WriteLine(a != b);
+            //System.Console.WriteLine(a < b);
+            //System.Console.WriteLine(a > b);
+            //System.Console.WriteLine(a >= b);
+            //System.Console.WriteLine(a <= b);
+
+            //System.Console.WriteLine(a + b);
+            //System.Console.WriteLine(a - b);
+            //System.Console.WriteLine(a * b);
+            //System.Console.WriteLine(a / b);
+            //System.Console.WriteLine(a % b);
+
+            //System.Console.WriteLine(sizeof(byte));
+            //System.Console.WriteLine(sizeof(char));
+            //System.Console.WriteLine(sizeof(short));
+            //System.Console.WriteLine(sizeof(int));
+            //System.Console.WriteLine(sizeof(long));
+            //System.Console.WriteLine(sizeof(float));
+            //System.Console.WriteLine(sizeof(double));
+
+            //char value1 = 'a' = 'A' + 32;
+            //char value2 = 'A';
+
+            //System.Console.WriteLine(value1 + value2);
+            //System.Console.WriteLine(value1 - value2);
+            //System.Console.WriteLine(value1 * value2);
+            //System.Console.WriteLine(value1 / value2);
+            //System.Console.WriteLine(value1 % value2);
+            //char value2 = '0';
+            //System.Console.WriteLine(value2 - 0);
+
+            //System.Console.WriteLine(sizeof(byte));
+            //System.Console.WriteLine(sizeof(char));
+            //System.Console.WriteLine(sizeof(int));
+            //System.Console.WriteLine(sizeof(long));
+            //System.Console.WriteLine(sizeof(float));
+            //System.Console.WriteLine(sizeof(double));
+
+            //                 0  1 2 3 4
+            //string message1 = "안녕하세요. 방갑습니다.";
+
+            //Console.WriteLine(message1[0]);
+            //Console.WriteLine(message1[1]);
+            //Console.WriteLine(message1[2]);
+            //Console.WriteLine(message1[3]);
+            //Console.WriteLine(message1[4]);
+
+            //int value3 = 12345;
+            //value3 += 200; Console.WriteLine(value3);
+            //value3 -= 200; Console.WriteLine(value3);
+            //value3 *= 200; Console.WriteLine(value3);
+            //value3 /= 200; Console.WriteLine(value3);
+            //value3 %= 200; Console.WriteLine(value3);
+
+            //string m1 = "Hello";
+            //m1 += " world";
+
+            //string resDir = "resource/";
+            //string textureFile = "m1.png";
+            //string texPath = resDir;
+            //texPath += textureFile;
+            //Console.WriteLine(texPath);
+
+            //// 단항 연산자
+            //int value = 100;
+            //int result = 0;
+
+            //result = ++value;
+            //Console.WriteLine($"result: {result} value: {value}");    // value += 1;
+
+            //result = value++;
+            //Console.WriteLine($"result: {result} value: {value}");    // value += 1;
+
+            //// value++;
+            //// result = value;
+            //// value = value+1;
+
+            //result = --value; Console.WriteLine($"result: {result} value: {value}");    // value -= 1;
+            //result = value--; Console.WriteLine($"result: {result} value: {value}");    // value -= 1;
+
+            //// value--;
+            //// result = value;
+            //// value = value-1;
+            ///
+            /// GetType
+
+            //Console.WriteLine(10.GetType());
+            //Console.WriteLine(10.0F.GetType());
+            //Console.WriteLine('한'.GetType());
+            //Console.WriteLine("message".GetType());
+
+            //int value = "안녕하세요";
+            //Console.WriteLine("value type: " + value.GetType());
+
+
+            long val10 = 0L        ; Console.WriteLine($"val10 type: { val10.GetType() }");
+            int val11 = 0          ; Console.WriteLine($"val11 type: {val11.GetType()}");
+            double val12 = 0.0     ; Console.WriteLine($"val12 type: { val12.GetType() }");
+            float val13 = 0.0F     ; Console.WriteLine($"val13 type: { val13.GetType() }");
+            char val14 = '가'      ; Console.WriteLine($"val14 type: { val14.GetType() }");
+            string val15 = "문자열"; Console.WriteLine($"val15 type: { val15.GetType() }");
+            float val16 = val13   ; Console.WriteLine($"val16 type: { val16.GetType() }");
 
         }
     }
---
+++ CsharpPractice03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsharpPractice03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var inputString = Console.ReadLine();
+            //Console.WriteLine(inputString);
+
+            var val1 = 1000;
+            var val2 = 2000;
+
+            long valc = val1 + val2;
+            float valf = valc;
+            double vald = valf;
+
+            valf = (float)vald;
+            valc = (long)valf;
+            val1 = (int)valc;
+
+            //var str = "20000";
+            //val1 = int.Parse(str);  Console.WriteLine(val1);
+            //valc = long.Parse(str);
+
+            //val1.ToString();
+            //valc.ToString();
+            //valf.ToString();
+            //vald.ToString();
+            //var str2 = val1 + ""; Console.WriteLine($"str2: type {str2.GetType()}");
+
+            //var b1 = bool.Parse("TRUE");  Console.WriteLine($"b1: type {b1.GetType()}: {b1}");
+            //var b2 = bool.Parse("False"); Console.WriteLine($"b2: type {b2.GetType()}: {b2}");
+            //var b3 = bool.Parse("tRUE");  Console.WriteLine($"b3: type {b3.GetType()}: {b3}");
+
+            int valMin = int.MinValue; Console.WriteLine(valMin);
+            valMin = -2147483648;
+            Console.WriteLine(-valMin);
+            //int valMax = int.MaxValue; Console.WriteLine(valMax);
+        }
+    }
+}
