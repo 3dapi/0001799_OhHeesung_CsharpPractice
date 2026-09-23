979e10efb25e6618289761b97050aba076b2ec5b
3dapi <emarhayyim@hotmail.com>
2026-09-23 12:02:22 +0900

    4장 연습문제풀이

---
+++ chapter04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace chapter04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 ~100 짝수 출력");
+            for (var i = 1; i <= 100; ++i)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("0 ~10 while 작성");
+            var start = 0;
+            while(start <=10)
+            {
+                Console.WriteLine(start);
+                ++start;
+            }
+        }
+    }
+}
